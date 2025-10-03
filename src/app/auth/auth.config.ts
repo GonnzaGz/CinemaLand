891e599776aff110ec7b0a77e7fb8ab0971307dc
GonnzaGz <94939097+GonnzaGz@users.noreply.github.com>
2025-10-02 23:44:47 -0300

    Feat: Cambiando variables del nuevo despliegue de amplify.

--- src/app/auth/auth.config.ts
+++ src/app/auth/auth.config.ts
@@ -4,7 +4,7 @@ export const authConfig: PassedInitialConfig = {
   config: {
     authority:
       'https://cognito-idp.us-east-1.amazonaws.com/us-east-1_3HspZNy7e',
-    redirectUrl: 'https://main.d1dbozfv7qh8jl.amplifyapp.com/',
+    redirectUrl: 'https://main.d229d9k3wl7093.amplifyapp.com/',
     clientId: '1v91jckl7411lmtn3r1k0m664p',
     scope: 'email openid phone',
     responseType: 'code',
